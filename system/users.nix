dc98d52378bdc58d64aa227062359366cea274fd
xMqme <140866184+xMqme@users.noreply.github.com>
2024-01-11 19:49:57 -0700

    a

--- system/users.nix
+++ system/users.nix
@@ -14,7 +14,8 @@
     #   /etc/nixos/ssh/authorized_keys
     # ];
     openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDJSzqlsE1s9Toudq7MjPJebqAjIJesgGS5TYHWbI/qfq31IEqEW1phvmeNmRXjdDMlJl05VjSEaGSaiNosS7TvTrjGYSVfYJ9Bw3oGszIWgLN4xxKNgGXEo5H+vhk4JVOMWcslWVSdSXAoD6ruBahaC08g74HxdPQo/NJ7v8kM2r/ZuLqu2rSHJAxcXvvPE2Zhnh6uHr+aSlNDweygg6Otk6fSA1tcGKaudW4Pjiob3olJ+x4WOppYNUBa9XOxMgX3MSEcr8XNdvsHOQBap6uF/m/Ph2Nl9fvlDcTWX5FxtP186B3c/Um3hTVLmuIgqbuAjhRfXuc8mtsDvGqTwH1wyV/R2todE4k88YDnt07GNdFD6/23q1n+MbAruY+FSuWVsnbgENG9G6Tlq6HMobLdvrUOF2OZVX4EWjya0a6HszycGIV9c9pmmVCmYEWzDtgEKisr7pZibbrcxB7M/4bY89t544Xux0XIetACPYXu2TEwglaRuIZd0n3dskbisrk= elliot@elliotpc"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDJSzqlsE1s9Toudq7MjPJebqAjIJesgGS5TYHWbI/qfq31IEqEW1phvmeNmRXjdDMlJl05VjSEaGSaiNosS7TvTrjGYSVfYJ9Bw3oGszIWgLN4xxKNgGXEo5H+vhk4JVOMWcslWVSdSXAoD6ruBahaC08g74HxdPQo/NJ7v8kM2r/ZuLqu2rSHJAxcXvvPE2Zhnh6uHr+aSlNDweygg6Otk6fSA1tcGKaudW4Pjiob3olJ+x4WOppYNUBa9XOxMgX3MSEcr8XNdvsHOQBap6uF/m/Ph2Nl9fvlDcTWX5FxtP186B3c/Um3hTVLmuIgqbuAjhRfXuc8mtsDvGqTwH1wyV/R2todE4k88YDnt07GNdFD6/23q1n+MbAruY+FSuWVsnbgENG9G6Tlq6HMobLdvrUOF2OZVX4EWjya0a6HszycGIV9c9pmmVCmYEWzDtgEKisr7pZibbrcxB7M/4bY89t544Xux0XIetACPYXu2TEwglaRuIZd0n3dskbisrk= elliot@elliotpc"     
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAHGLDrHkNi0DSxFVENxISi2qo2x1GBGIifRINarFV/m 109875196+caernarferon@users.noreply.github.com"
     ];
   };
   users = {
