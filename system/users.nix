f429defb704d622207eb82634cde4de1e3851f76
xMqme <140866184+xMqme@users.noreply.github.com>
2024-01-11 22:39:18 -0700

    Update users.nix

--- system/users.nix
+++ system/users.nix
@@ -16,6 +16,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDJSzqlsE1s9Toudq7MjPJebqAjIJesgGS5TYHWbI/qfq31IEqEW1phvmeNmRXjdDMlJl05VjSEaGSaiNosS7TvTrjGYSVfYJ9Bw3oGszIWgLN4xxKNgGXEo5H+vhk4JVOMWcslWVSdSXAoD6ruBahaC08g74HxdPQo/NJ7v8kM2r/ZuLqu2rSHJAxcXvvPE2Zhnh6uHr+aSlNDweygg6Otk6fSA1tcGKaudW4Pjiob3olJ+x4WOppYNUBa9XOxMgX3MSEcr8XNdvsHOQBap6uF/m/Ph2Nl9fvlDcTWX5FxtP186B3c/Um3hTVLmuIgqbuAjhRfXuc8mtsDvGqTwH1wyV/R2todE4k88YDnt07GNdFD6/23q1n+MbAruY+FSuWVsnbgENG9G6Tlq6HMobLdvrUOF2OZVX4EWjya0a6HszycGIV9c9pmmVCmYEWzDtgEKisr7pZibbrcxB7M/4bY89t544Xux0XIetACPYXu2TEwglaRuIZd0n3dskbisrk= elliot@elliotpc"     
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAHGLDrHkNi0DSxFVENxISi2qo2x1GBGIifRINarFV/m 109875196+caernarferon@users.noreply.github.com"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICEvnXyYl6HjmhQ1e5nSrp+JELO4xWTo8oVZYssZ3cW4 nixer@nixerpc"
     ];
   };
   users = {
