5283d26a0f946adefd6795325b55c29e65c9dca2
spacefault <74156492+spacefault@users.noreply.github.com>
2024-01-13 18:02:35 -0700

    system: potential pufferpanel patch

--- system/services.nix
+++ system/services.nix
@@ -18,7 +18,7 @@
       enable = true;
       extraPackages = with pkgs; [ bash curl gawk gnutar gzip ];
       package = pkgs.buildFHSEnv {
-        name = "pufferpanel-fhs";
+        name = "pufferpanel";
         runScript = lib.getExe pkgs.pufferpanel;
         targetPkgs = pkgs': with pkgs'; [ icu openssl zlib ];
       };
