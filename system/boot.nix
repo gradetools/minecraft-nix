9ec4f80e358c61d14e61aa40823286f9f7d6b8e7
xMqme <140866184+xMqme@users.noreply.github.com>
2024-01-18 18:02:17 -0700

    Update boot.nix

--- system/boot.nix
+++ system/boot.nix
@@ -5,6 +5,7 @@
 }: {
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
+    kernel.sysctl."net.ipv6.conf.eth0.disable_ipv6" = true;
     loader = {
       systemd-boot.enable = false;
       efi.canTouchEfiVariables = true;
